80dcf13c4c26470bf231ecae5bb09a906b2d5124
Rafael Pierre <rafaelpierre@github.com>
2025-01-22 13:14:03 +0100

    init

---
+++ src/text_in_audio_out.py
@@ -0,0 +1,52 @@
+import base64
+import asyncio
+import os
+from azure.identity.aio import DefaultAzureCredential
+from rtclient import (
+    ResponseCreateMessage,
+    RTLowLevelClient,
+    ResponseCreateParams
+)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Set environment variables or edit the corresponding values here.
+endpoint = os.environ["AZURE_OPENAI_ENDPOINT"]
+deployment = "gpt-4o-realtime-preview"
+
+async def text_in_audio_out():
+    async with RTLowLevelClient(
+        url=endpoint,
+        azure_deployment=deployment,
+        token_credential=DefaultAzureCredential(),
+    ) as client:
+        await client.send(
+            ResponseCreateMessage(
+                response=ResponseCreateParams(
+                    modalities={"audio", "text"}, 
+                    instructions="Please assist the user."
+                )
+            )
+        )
+        done = False
+        while not done:
+            message = await client.recv()
+            match message.type:
+                case "response.done":
+                    done = True
+                case "error":
+                    done = True
+                    print(message.error)
+                case "response.audio_transcript.delta":
+                    print(f"Received text delta: {message.delta}")
+                case "response.audio.delta":
+                    buffer = base64.b64decode(message.delta)
+                    print(f"Received {len(buffer)} bytes of audio data.")
+                case _:
+                    pass
+
+async def main():
+    await text_in_audio_out()
+
+asyncio.run(main())
